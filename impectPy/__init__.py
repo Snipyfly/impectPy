8bd0911379db31683fae3be9976b7113184f9db8
Snipyfly <simon.hofmann1@gmx.de>
2025-11-11 15:41:42 +0100

    Fix xml module name conflict

--- impectPy/__init__.py
+++ impectPy/__init__.py
@@ -1,6 +1,9 @@
 # define version attribute
 __version__ = "2.5.2"
 
+import sys
+from types import ModuleType
+
 # import modules
 from .access_token import getAccessToken
 from .iterations import getIterations
@@ -11,10 +14,16 @@ from .iteration_averages import getPlayerIterationAverages, getSquadIterationAve
 from .player_scores import getPlayerMatchScores, getPlayerIterationScores
 from .squad_scores import getSquadMatchScores, getSquadIterationScores
 from .player_profile_scores import getPlayerProfileScores
-from .xml import generateXML
+from .xml_generator import generateXML
 from .set_pieces import getSetPieces
 from .squad_ratings import getSquadRatings
 from .squad_coefficients import getSquadCoefficients
 from .match_info import getFormations, getSubstitutions, getStartingPositions
 from .config import Config as Config
 from .impect import Impect as Impect
+
+
+if "impectPy.xml" not in sys.modules:
+    _xml_module = ModuleType("impectPy.xml")
+    _xml_module.generateXML = generateXML
+    sys.modules["impectPy.xml"] = _xml_module
